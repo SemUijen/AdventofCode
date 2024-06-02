8ef92bb36faf251254ba00bd19fdbc1f7c4df83a
SemUijen <grootveer.sem@outlook.com>
2024-06-02 21:13:35 +0200

    added day1 part 2 test

--- Advent2023/test/test_day1.py
+++ Advent2023/test/test_day1.py
@@ -10,3 +10,10 @@ def test_day1_part1():
     assert solve_day1(test_data) == 142
     assert solve_day1(data) == 54644
 
+def test_day1_part2():
+
+    test_data, data = read(day=1, is_test=True), read(day=1)
+
+    ## Part1
+    assert solve_day1(test_data) == 142
+    assert solve_day1(data) == 54644
