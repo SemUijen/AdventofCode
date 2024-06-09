ac33bf6f75cbf00b21a68ded95c6e472f6c06404
SemUijen <grootveer.sem@outlook.com>
2024-06-09 17:20:33 +0200

    added day 2 part 2

--- Advent2023/test/test_day2.py
+++ Advent2023/test/test_day2.py
@@ -2,7 +2,7 @@ import pytest
 from advent.day2 import solve_day2, solve_day2_part2
 from advent.read import read
 
-def test_day1_part1():
+def test_day2_part1():
 
 
     test_data, data = read(day=2, part=1, is_test=True), read(day=2, part=1)
@@ -10,4 +10,17 @@ def test_day1_part1():
     ## Part1
     assert solve_day2(test_data) == 8
     assert solve_day2(data) == 2879
+
+
+
+def test_day2_part2():
+
+
+    test_data, data = read(day=2, part=1, is_test=True), read(day=2, part=1)
+
+    ## Part1
+    assert solve_day2_part2(test_data) == 2286
+    assert solve_day2_part2(data) == 65122
+
+
 
